43bacf9561d6a41a7fe959a64ca5a1f792ceffd9
halim <haleemyoosuph@gmail.com>
2020-10-26 01:55:14 +0100

    fix: fixed async await promises issue

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,51 +2,71 @@ const User = require('../models/User');
 const { pick } = require('../utils/helperFunctions');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const { JwtSecret } = require('../config/index');
-const wrapper = require('../utils/wrapper');
+const { JwtSecret } = require('../config');
 const saltRounds = 10;
 
-const register = async (req, res) => {
-  try {
-    req.body = { ...req.body };
-    let data = pick(req.body, 'password', 'email');
-    //   will need to add validation
-    const hash = await bcrypt.hash(data.password, saltRounds);
-    data.password = hash;
-    let user = new User(data);
-    await user.save();
-    var token = jwt.sign({ _id: user._id, isAdmin: user.isAdmin }, JwtSecret);
-    console.log(token);
-    return res.send({ data: { token }, message: 'User signup successful' });
-  } catch (error) {
-    console.log(error);
-  }
+const register = async (req, res, next) => {
+  req.body = { ...req.body };
+  let data = pick(req.body, 'password', 'email');
+  //   will need to add validation
+  let hash = null;
+  bcrypt
+    .hash(data.password, saltRounds)
+    .then((r) => {
+      hash = r;
+      data.password = hash;
+      console.log(hash);
+      let user = new User(data);
+      user
+        .save()
+        .then((savedUser) => {
+          var token = jwt.sign(
+            { _id: user._id, isAdmin: user.isAdmin },
+            JwtSecret
+          );
+          return res.send({
+            data: { token },
+            message: 'User signup successful',
+          });
+        })
+        .catch((err) => {
+          return res.status(400).json({ error: 'email exists' });
+        });
+    })
+    .catch((err) => {
+      next(err);
+    });
   //   res.render('register');
 };
 
-const login = async (req, res) => {
-  try {
-    req.body = { ...req.body };
-    let data = pick(req.body, 'password', 'email');
-    const user = await User.findOne({ email: data.email });
-    if (user === null) {
-      return res
-        .status(400)
-        .json({ error: 'Account with the email exist does not exist' });
-    }
-    const compare = await bcrypt.compare(data.password, user.password);
-    if (!compare) {
-      return res.status(400).send({ error: 'Incorrect password' });
-    }
-    var token = jwt.sign({ _id: user._id, isAdmin: user.isAdmin }, JwtSecret);
-    console.log(token);
-    return res.send({
-      data: { token },
-      message: "message: 'User login successful'",
-    });
-  } catch (error) {
-    console.log(error);
-  }
+const login = (req, res, next) => {
+  req.body = { ...req.body };
+  let data = pick(req.body, 'password', 'email');
+  User.findOne({ email: data.email })
+    .then((user) => {
+      if (user === null) {
+        return res
+          .status(400)
+          .json({ error: 'Account with the email exist does not exist' });
+      }
+      bcrypt
+        .compare(data.password, user.password)
+        .then((compare) => {
+          if (!compare) {
+            return res.status(400).send({ error: 'Incorrect password' });
+          }
+          var token = jwt.sign(
+            { _id: user._id, isAdmin: user.isAdmin },
+            JwtSecret
+          );
+          return res.send({
+            data: { token },
+            message: "message: 'User login successful'",
+          });
+        })
+        .catch((err) => next(err));
+    })
+    .catch((err) => next(err));
 };
 
 module.exports = { register, login };
