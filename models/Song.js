67b96b93eba5a889f85f4cf4e23ed79ea8e50fca
Zubs <zubairidrisaweda@gmail.com>
2020-11-01 20:07:26 +0100

    Check out my songs schema

--- models/Song.js
+++ models/Song.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 
-const songSchema = mongoose.Schema({});
+const songSchema = mongoose.Schema({
+	title: {
+		type: String,
+	},
+	cover_image: String,	// I don't know how to store images.
+	audio_file: String,		// I don't know how to store audio too.
+	user_id: String,		// For model relations. 🤲
+}, { timestamps: true });
 
 const songs = mongoose.model('songs', songSchema);
 
